63bdc263b352b64c08e786ff041d9410b7e1e77d
ahlan02 <allan.flores2002@yahoo.com>
2023-02-25 19:27:05 -0800

    added motor init func

--- stepper/bot.cpp
+++ stepper/bot.cpp
@@ -1,27 +1,20 @@
 #include <Stepper.h>
 #include "bot.h"
 
-/*typedef struct bot{
-  float wheelcircum;
-  uint8_t totalsteps;
-  bool FLO,FRO,BLO,BRO;
-  Stepper motorFL(totalsteps,22,24,26,28);
-  motorFL.setSpeed(15);
-  Stepper motorFR(totalsteps,23,25,27,29);
-  motorFR.setSpeed(15);
-  Stepper motorBR(totalsteps,30,32,34,36);
-  motorBL.setSpeed(15);
-  Stepper motorBL(totalsteps,31,33,35,37);
-  motorBR.setSpeed(15);
-}bot;
-*/
-
-void rotateBot(&bot, int8_t deg){
+void rotateBot(bot, int8_t deg){
 
 }
 
-void driveBot(&bot, uint8_t steps){
-  motorFL.
+void driveBot(bot, uint8_t steps){
   
+}
+
+void Motor_Init(bool O, String MotorName, uint8_t P1, uint8_t P2, uint8_t P3, uint8_t P4, uint8_t steps){
+  if(O == 0){
+    Stepper MotorName(steps,P1,P2,P3,P4);
+  }
+  elif(O == 1){
+    Stepper MotorName(steps,P4,P3,P2,P1);
+  }
 }
 
--- stepper/bot.h
+++ stepper/bot.h
@@ -1,19 +1,21 @@
 #ifndef BOT_H
 #define BOT_H
 
-#define STEPS 100;
+#include "Arduino.h"
+#include "Stepper.h" 
 
-//typedef struct bot;
+
 class bot{
   public:
   bot(uint8_t wheelcirc, uint8_t steps);
-  Stepper FL(STEPS,22,24,26,28);
-  Stepper FR(STEPS,23,25,27,29);
-  Stepper BL(STEPS,30,32,34,36);
-  Stepper BR(STEPS,31,33,35,37);
-  void rotateBot(&bot, int8_t deg);
-  void driveBot(&bot, uint8_t steps);
-
+  void rotateBot(bot, int8_t deg);
+  void driveBot(bot, uint8_t steps);
+  void Motor_Init(bool O, string MotorName, uint8_t P1, uint8_t P2, uint8_t P3, uint8_t P4, uint8_t steps);
+  Motor_Init(0,FR,22,24,26,28,100);
+  Motor_Init(1,FL,23,25,27,29,100);
+  Motor_Init(0,BR,30,32,34,36,100);
+  Motor_Init(1,BL,31,33,35,37,100);
+  
   private:
     uint8_t wheelcirc;
     uint8_t steps;
